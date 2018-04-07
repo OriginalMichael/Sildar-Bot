0a2cee34cc375b7b595dac0acc4452dcc0ea65b5
OriginalMichael <16965194+OriginalMichael@users.noreply.github.com>
2018-04-06 20:38:23 -0400

    Refactor

--- sildar.js
+++ sildar.js
@@ -22,79 +22,16 @@ module.exports.start = () => {
 
   bot.on('message', function (user, userID, channelID, message, evt) {
     try {
-      if (message.match(/!roll d\d+/i)) {
-        const max = message.match(/!roll d(\d+)/i)[1];
-        const result = (Math.floor((Math.random() * max)) + 1).toString();
-        bot.sendMessage({
-          to: channelID,
-          message: `${user} rolls a ${result}!`,
-        }); 
-        return;
-      }
-      if (message.match(/!roll \d+d\d+/i)) {
-        const match = message.match(/!roll (\d+)d(\d+)/i);
-        const num = match[1];
-        const max = match[2];
-        const result = [];
-        let sum = 0;
-        for (let i = 0; i < num; i++) {
-          const val = Math.floor((Math.random() * max)) + 1;
-          result.push(val);
-          sum += val;
-        }
-        bot.sendMessage({
-          to: channelID,
-          message: `${user} rolls ${result} = ${sum}!`.replace(/,/g, ' + '),
-        }); 
-        return;
-      }
-      if (message.match(/!proll d\d+/i)) {
-        const max = message.match(/!proll d(\d+)/i)[1];
-        const result = (Math.floor((Math.random() * max)) + 1).toString();
-        bot.sendMessage({
-          to: userID,
-          message: `${user} rolls a ${result}!`,
-        }); 
-        return;
-      }
-      if (message.match(/!proll \d+d\d+/i)) {
-        const match = message.match(/!proll (\d+)d(\d+)/i);
-        const num = match[1];
-        const max = match[2];
-        const result = [];
-        let sum = 0;
-        for (let i = 0; i < num; i++) {
-          const val = Math.floor((Math.random() * max)) + 1;
-          result.push(val);
-          sum += val;
-        }
-        bot.sendMessage({
-          to: userID,
-          message: `${user} rolls ${result} = ${sum}!`.replace(/,/g, ' + '),
-        }); 
-        return;
-      }
-      if (message.match(/!sildar roll d\d+/i)) {
-        bot.sendMessage({
-          to: channelID,
-          message: 'I roll a 1! I Suck!',
-        }); 
-        return;    
-      }
-      if (message.match(/!sildar roll \d+d\d+/i)) {
-        bot.sendMessage({
-          to: channelID,
-          message: 'I somehow roll a 1! I Suck!',
-        }); 
-        return;
-      }
-      if (message.match(/!varjil/i)) {
-         bot.sendMessage({
-          to: channelID,
-          message: 'I am black. <:varjilisblack:431658564101210122>',
-        }); 
-        return;
-      }
+      if (message.match(/!commands/i)) return listCommands(channelID);
+      if (message.match(/!roll d\d+/i)) return rollSingle(user, channelID, message);
+      if (message.match(/!roll \d+d\d+/i)) return rollMultiple(user, channelID, message);
+      if (message.match(/!proll d\d+/i)) return privateRollSingle(userID, message);
+      if (message.match(/!proll \d+d\d+/i)) return privateRollMultiple(userID, message);
+      if (message.match(/!sildar roll d\d+/i)) return sildarRollSingle(channelID);
+      if (message.match(/!sildar roll \d+d\d+/i)) return sildarRollMultiple(channelID, message);
+      if (message.match(/!sildar/i)) return sildarStatus(channelID);
+      if (message.match(/!varjil/i)) return varjilQuotes(channelID);
+      if (message.match(/!ilthar/i)) return iltharQuotes(channelID);
     } catch (err) {
       console.log(err);
       bot.sendMessage({
@@ -103,4 +40,122 @@ module.exports.start = () => {
       }); 
     }
   });
+
+  const listCommands = (channelID) => {
+    const list = [
+      '!commands',
+      '!roll dxx',
+      '!roll xdxx',
+      '!proll dxx',
+      '!proll xdxx',
+      '!sildar roll dxx',
+      '!sildar roll xdxx',
+      '!sildar',
+      '!varjil',
+      '!ilthar',
+    ];
+    const commands = `${list}`.replace(/,/g, '\n');
+    bot.sendMessage({
+      to: channelID,
+      message: commands,
+    }); 
+  }
+
+  const rollSingle = (user, channelID, message) => {
+    const max = message.match(/!roll d(\d+)/i)[1];
+    const result = (Math.floor((Math.random() * max)) + 1).toString();
+    bot.sendMessage({
+      to: channelID,
+      message: `${user} rolls a ${result}!`,
+    });
+  }
+
+  const privateRollSingle = (userID, message) => {
+    const max = message.match(/!proll d(\d+)/i)[1];
+    const result = (Math.floor((Math.random() * max)) + 1).toString();
+    bot.sendMessage({
+      to: userID,
+      message: `You roll a ${result}!`,
+    });
+  }
+
+  const rollMultiple = (user, channelID, message) => {
+    const match = message.match(/!roll (\d+)d(\d+)/i);
+    const num = match[1];
+    const max = match[2];
+    const result = [];
+    let sum = 0;
+    if (num === '1') return rollSingle(user, channelID, message);
+    for (let i = 0; i < num; i++) {
+      const val = Math.floor((Math.random() * max)) + 1;
+      result.push(val);
+      sum += val;
+    }
+    bot.sendMessage({
+      to: channelID,
+      message: `${user} rolls ${result} = ${sum}!`.replace(/,/g, ' + '),
+    }); 
+  }
+
+  const privateRollMultiple = (userID, message) => {
+    const match = message.match(/!proll (\d+)d(\d+)/i);
+    const num = match[1];
+    const max = match[2];
+    const result = [];
+    let sum = 0;
+    if (num === '1') return privateRollSingle(userID, message);
+    for (let i = 0; i < num; i++) {
+      const val = Math.floor((Math.random() * max)) + 1;
+      result.push(val);
+      sum += val;
+    }
+    bot.sendMessage({
+      to: userID,
+      message: `You roll ${result} = ${sum}!`.replace(/,/g, ' + '),
+    });
+  }
+
+  const sildarRollSingle = (channelID) => {
+    bot.sendMessage({
+      to: channelID,
+      message: 'I roll a 1! I Suck!',
+    });
+  }
+
+  const sildarRollMultiple = (channelID, message) => {
+    if (message.match(/!sildar roll 1d\d+/i)) return sildarRollSingle(channelID);
+    bot.sendMessage({
+      to: channelID,
+      message: 'I somehow roll a 1! I Suck!',
+    });
+  }
+
+  const sildarStatus = (channelID) => {
+    bot.sendMessage({
+      to: channelID,
+      message: 'Sildar is up and running!',
+    });
+  }
+
+  const varjilQuotes = (channelID) => {
+    bot.sendMessage({
+      to: channelID,
+      message: 'I am black. <:varjilisblack:431658564101210122>',
+    });
+  }
+
+  const iltharQuotes = (channelID) => {
+    const list = [
+      'Do you have burgers?',
+      'I want a fifteen feet long two by four.',
+      'Here, have 20 gold.',
+      'I will have one of everything. Bring it to my room.',
+    ];
+    const random = Math.floor((Math.random() * list.length));
+    const quote = `${list[random]} - Ilthar~`;
+    bot.sendMessage({
+      to: channelID,
+      message: quote,
+    });
+  }
 }
